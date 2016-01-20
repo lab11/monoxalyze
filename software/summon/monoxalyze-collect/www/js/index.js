3f6640f001c3cac3fdfc8ed31066c70b103b4dc8
Josh Adkins <adkinsjd@umich.edu>
2016-01-20 15:39:48 -0500

    added the connect function and tried to log device state

--- software/summon/monoxalyze-collect/www/js/index.js
+++ software/summon/monoxalyze-collect/www/js/index.js
@@ -2,9 +2,11 @@
 
 var deviceId = "C0:98:E5:30:03:14";                                                 // while testing, replace with address of a BLE peripheral
 var deviceName = "BLE Device";                                                      // while testing, replace with desired name
-var serviceUuid = "1800";                                                           // example service UUID to access
-var characteristicUuid = "2A00";                                                    // example characteristic UUID to read or write
-var writeValue = "Written Name";                                                    // value to write to characteristic
+var serviceId = "1800"; 
+var startCollectId = "2A00"; 
+var stopCollectId = "2A00"; 
+var startReadId = "2A00"; 
+var doneReadId = "2A00"; 
 
 var app = {
     // Application Constructor
@@ -15,48 +17,55 @@ var app = {
     },
     // App Ready Event Handler
     onAppReady: function() {
-        if (window.gateway) {                                                       // if UI opened through Summon,
-            deviceId = window.gateway.getDeviceId();                                // get device ID from Summon
-            deviceName = window.gateway.getDeviceName();                            // get device name from Summon
+        if (window.gateway) { // if UI opened through Summon,
+            deviceId = window.gateway.getDeviceId();// get device ID from Summon
+            deviceName = window.gateway.getDeviceName();//e name from Summon
         }
-        ble.isEnabled(app.onEnable);                                                // if BLE enabled, goto: onEnable
+        ble.isEnabled(app.onEnable);  // if BLE enabled, goto: onEnable
     },
+
     // App Paused Event Handler
-    onPause: function() {                                                           // if user leaves app, stop BLE
+    onPause: function() {    // if user leaves app, stop BLE
         ble.disconnect(deviceId);
         ble.stopScan();
     },
+
     // Bluetooth Enabled Callback
     onEnable: function() {
-        app.onPause();                                                              // halt any previously running BLE processes
-        ble.startScan([], app.onDiscover, app.onAppReady);                          // start BLE scan; if device discovered, goto: onDiscover
+        app.onPause();         // halt any previously running BLE processes
+        ble.startScan([], app.onDiscover, app.onAppReady);     
         app.log("Searching for " + deviceName + " (" + deviceId + ").");
     },
     // BLE Device Discovered Callback
     onDiscover: function(device) {
         if (device.id == deviceId) {
             app.log("Found " + deviceName + " (" + deviceId + ")! Connecting.");
-            ble.connect(deviceId, app.onConnect, app.onAppReady);                   // if device matches, connect; if connected, goto: onConnect
+            ble.connect(deviceId, app.onConnect, app.onAppReady);
         }
     },
+
     // BLE Device Connected Callback
     onConnect: function(device) {
         app.log("Connected to " + deviceName + " (" + deviceId + ")!");
-        // uncomment to read characteristic on connect; if read is good, goto: onRead
-        ble.read(deviceId, serviceUuid, characteristicUuid, app.onRead, app.onError);  
-        // uncomment to write writeValue to characteristic on connect; if write is good, goto: onWrite
-        // ble.write(deviceId, serviceUuid, characteristicUuid, app.stringToBytes(writeValue), app.onWrite, app.onError); 
+		app.log("device
+		//change the dome name to say connected and ready to collect data
+		document.getElementById("collect_button").innerHTML = "Start Collecting";
+		document.getElementById("collect_button").disabled = false;
+
+		//serviceId = device.services[2];
+		
+        //ble.read(deviceId, serviceUuid, characteristicUuid, app.onRead, app.onError);  
+        //ble.write(deviceId, serviceUuid, characteristicUuid, app.stringToBytes(writeValue), app.onWrite, app.onError); 
     },
-    // BLE Characteristic Read Callback
     onRead: function(data) {
-        app.log("Characteristic Read: " + app.bytesToString(data));                 // display read value as string
+        app.log("Characteristic Read: " + app.bytesToString(data));      
     },
     // BLE Characteristic Write Callback
     onWrite : function() {
-        app.log("Characeristic Written: " + writeValue);                            // display write success
+        app.log("Characeristic Written: " + writeValue);      
     },
-    // BLE Characteristic Read/Write Error Callback
-    onError: function() {                                                           // on error, try restarting BLE
+
+    onError: function() {              
         app.log("Read/Write Error.")
         ble.isEnabled(deviceId,function(){},app.onAppReady);
         ble.isConnected(deviceId,function(){},app.onAppReady);
@@ -71,6 +80,10 @@ var app = {
     bytesToString: function(buffer) {
         return String.fromCharCode.apply(null, new Uint8Array(buffer));
     },
+	collect_button: function() {
+		app.log("collect button pressed");
+		ble.write(deviceId, service
+	}
     // Function to Log Text to Screen
     log: function(string) {
         document.querySelector("#console").innerHTML += (new Date()).toLocaleTimeString() + " : " + string + "<br />"; 
